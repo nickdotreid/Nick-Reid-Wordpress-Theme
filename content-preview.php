5e5c54f6b701713388ddb3de32a2eba424b71338
Nick Reid <nickreid@nickreid.com>
2011-09-11 00:34:21 -0700

    making common post styles

--- content-preview.php
+++ content-preview.php
@@ -4,7 +4,7 @@
 
 <div id="post-<?php the_ID(); ?>" <?php post_class('preview'); ?>>
 	<div class="entry-header">
-		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'toolbox' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+		<h3 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'toolbox' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
 	</div><!-- .entry-header -->
 	<?	if ( has_post_thumbnail() ):	?>
 	<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'toolbox' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark">
